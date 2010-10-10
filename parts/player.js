8a97b4f38b66df37e69a8d155891c6dd02b4b718
Tomasz bla Fortuna <bla@thera.be>
2010-10-10 11:28:44 +0200

    HTML5 works

---
+++ parts/player.js
@@ -0,0 +1,80 @@
+var presData = null;
+var currentIndex = null;
+
+function log(txt) {
+    $('#log').append(txt);
+}
+
+function loadFrame(index) 
+{
+    if (currentIndex == index)
+        return; /* Nothing to do */
+    currentIndex = index;
+
+    var file = 'prs/' + presData[index][1];
+
+    $('#presentation img').attr('src', file);
+}
+
+function updatePresentation() {
+
+    var v = $('video');
+    var t = v[0].currentTime;
+
+    if (presData == null) {
+        /* Ignore */
+        setTimeout('updatePresentation();', 1000);
+    }
+
+    var newFrameIndex = null;
+
+    for (idx in presData) {
+        if (!newFrameIndex) {
+            newFrameIndex = idx;
+            continue;
+        }
+
+        var frameTime = presData[idx][0];
+
+        if (t > frameTime) {
+            newFrameIndex = idx;
+        } else
+            break;
+    }
+    
+    loadFrame(newFrameIndex);
+
+    setTimeout('updatePresentation();', 1000);
+}
+
+
+
+
+$(document).ready(
+    function () {
+        function getPresData(data) {
+            presData = data;
+            loadFrame(0);
+        }
+
+
+        $.ajax({
+            url: 'data.json',
+            dataType: 'json',
+            success: getPresData,
+            
+            error: function () { 
+                log('Error: Unable to read presentation calibration data (data.json)'); 
+            } 
+        });
+
+
+        $('video').bind('seeked', 
+                        function () {
+                            updatePresentation();
+                        }
+                       );
+
+        setTimeout('updatePresentation();', 1000);
+    }
+);
